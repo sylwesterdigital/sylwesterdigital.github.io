ca7e58931aec71992c678741aee051123ff3912d
Sylwester Mielniczuk <flaboy.com@gmail.com>
2024-03-25 00:14:41 +0000

    wss url update

--- js/ws-client.js
+++ js/ws-client.js
@@ -362,7 +362,7 @@ function initWS() {
     // const wsUrl = 'wss://37.27.5.200:4444'
     // const wsUrl = 'wss://yolo.cx:4444'
     // const wsUrl = 'wss://wss.mielniczuk.com';
-    const wsUrl = 'wss://analytics.mielniczuk.com:4444';
+    const wsUrl = 'wss://analytics.mielniczuk.com/websocket';
 
 
     ws = new WebSocket(wsUrl);
